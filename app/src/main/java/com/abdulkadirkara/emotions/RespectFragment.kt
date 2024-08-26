7545220495b793ed0b62745c47b03a474607fe4c
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-08-26 20:45:11 +0300

    fragments created

---
+++ app/src/main/java/com/abdulkadirkara/emotions/RespectFragment.kt
@@ -0,0 +1,18 @@
+package com.abdulkadirkara.emotions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RespectFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_respect, container, false)
+    }
+}
