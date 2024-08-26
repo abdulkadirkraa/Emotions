e30634744b32266c100865dd5c5b39d91b757dc4
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-08-27 01:28:53 +0300

    bottom navigation view created

--- app/src/main/java/com/abdulkadirkara/emotions/view/MainActivity.kt
+++ app/src/main/java/com/abdulkadirkara/emotions/view/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.abdulkadirkara.emotions.R
 import com.abdulkadirkara.emotions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,5 +26,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.apply {
+            setContentView(root)
+            setSupportActionBar(toolBar)
+            navController = (supportFragmentManager
+                .findFragmentById(navHostFragment.id) as NavHostFragment)
+                .navController
+        }
+
     }
 }
