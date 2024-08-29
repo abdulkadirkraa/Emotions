53bef92ba0a5732743205a54dc22ed8a5cf4f82c
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-08-29 19:10:17 +0300

    logs were added

--- app/src/main/java/com/abdulkadirkara/emotions/view/HomeFragment.kt
+++ app/src/main/java/com/abdulkadirkara/emotions/view/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.abdulkadirkara.emotions.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,6 +24,7 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.e("EGO","HomeFragment-onCreateView")
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         // Eğer rootView null ise, yeni bir View şişiriyoruz ve binding'i başlatıyoruz
         if (rootView == null) {
@@ -37,6 +39,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        Log.e("EGO","HomeFragment-onViewCreated rootView: ${rootView.toString()}")
         setupSwitches()
         observeViewModel()
         setupSwitchListeners()
@@ -53,28 +56,15 @@ class HomeFragment : Fragment() {
             switchKindness.isChecked = false
             switchGiving.isChecked = false
             switchRespect.isChecked = false
+            Log.e("EGO","HomeFragment-setupSwitches switchEgo: ${switchEgo.isChecked}")
+            Log.e("EGO","HomeFragment-setupSwitches switchRespect: ${switchRespect.isChecked}")
         }
     }
 
     private fun onEgoCheckedChanged(isChecked: Boolean){
         binding.switchEgo.isChecked = isChecked
+        Log.e("EGO","HomeFragment-onEgoCheckedChanged isChecked: $isChecked")
         updateBottomNavigationView()
-        if (rootView != null && isChecked){
-            setSwitchesChecked(false)
-            setSwitchesClickable(false)
-            observeisEgoChecked()
-            observeAreSwitchesChecked()
-            observeAreSwitchesClickable()
-        }
-    }
-    private fun onSwitchesClikcableChanged(isClickable: Boolean){
-        setSwitchesClickable(isClickable)
-    }
-    private fun onSwitchesCheckedChanged(isChecked: Boolean){
-        setSwitchesChecked(isChecked)
-    }
-    private fun onNavigationItemsChanged(items: List<Int>){
-        updateBottomNavigationView(items)
     }
     private fun onMaxItemsReachedChanged(isMaxReached: Boolean) {
         if (isMaxReached) {
@@ -84,22 +74,26 @@ class HomeFragment : Fragment() {
     }
 
     private fun observeisEgoChecked() {
+        Log.e("EGO","HomeFragment-observeisEgoChecked")
         viewModel.isEgoChecked.observe(viewLifecycleOwner,::onEgoCheckedChanged)
     }
     private fun observeAreSwitchesClickable() {
-        viewModel.areSwitchesClickable.observe(viewLifecycleOwner, ::onSwitchesClikcableChanged)
+        Log.e("EGO","HomeFragment-observeAreSwitchesClickable")
+        viewModel.areSwitchesClickable.observe(viewLifecycleOwner, ::setSwitchesClickable)
     }
     private fun observeAreSwitchesChecked() {
-        viewModel.areSwitchesChecked.observe(viewLifecycleOwner, ::onSwitchesCheckedChanged)
+        Log.e("EGO","HomeFragment-observeAreSwitchesChecked")
+        viewModel.areSwitchesChecked.observe(viewLifecycleOwner, ::setSwitchesChecked)
     }
     private fun observeNavigationItems() {
-        viewModel.navigationItems.observe(viewLifecycleOwner, ::onNavigationItemsChanged)
+        viewModel.navigationItems.observe(viewLifecycleOwner, ::updateBottomNavigationView)
     }
     private fun observeMaxItemsReachedMessage() {
         viewModel.maxItemsReachedMessage.observe(viewLifecycleOwner, ::onMaxItemsReachedChanged)
     }
 
     private fun observeViewModel() {
+        Log.e("EGO","HomeFragment-observeViewModel")
         observeisEgoChecked()
         observeAreSwitchesClickable()
         observeAreSwitchesChecked()
@@ -110,27 +104,11 @@ class HomeFragment : Fragment() {
     private fun setupSwitchEgoListener() {
         binding.switchEgo.setOnCheckedChangeListener { _, isChecked ->
             viewModel.onEgoSwitchChanged(isChecked)
-            // switchEgo değiştiğinde diğer switch'lerin durumunu güncelle
-            with(binding) {
-                val switches = listOf(switchHappiness, switchOptimism, switchKindness, switchGiving, switchRespect)
-                switches.forEach { switch ->
-                    if (isChecked) {
-                        switch.isChecked = false
-                        switch.isClickable = false
-                        viewModel.onOtherSwitchChanged(switch.id, false)
-                        setSwitchesChecked(false)
-                        setSwitchesClickable(false)
-                        observeisEgoChecked()
-                        observeAreSwitchesChecked()
-                        observeAreSwitchesClickable()
-
-                        updateBottomNavigationView()
-                    } else {
-                        if (!switch.isChecked) {
-                            viewModel.onOtherSwitchChanged(switch.id, false)
-                        }
-                    }
-                }
+            Log.e("EGO","HomeFragment-setupSwitchEgoListener isChecked: $isChecked")
+            if (isChecked){
+                setSwitchesClickable(false)
+                setSwitchesChecked(false)
+                updateBottomNavigationView()
             }
         }
     }
@@ -157,10 +135,12 @@ class HomeFragment : Fragment() {
     private fun setupSwitchRespectListener(){
         binding.switchRespect.setOnCheckedChangeListener { _, isChecked ->
             viewModel.onOtherSwitchChanged(R.id.respectFragment, isChecked)
+            Log.e("EGO","HomeFragment-setupSwitchRespectListener isChecked: $isChecked")
         }
     }
 
     private fun setupSwitchListeners() {
+        Log.e("EGO","HomeFragment-setupSwitchListeners")
         setupSwitchEgoListener()
         setupSwitchHappinessListener()
         setupSwitchOptimismListener()
@@ -176,6 +156,7 @@ class HomeFragment : Fragment() {
             switchKindness.isClickable = isClickable
             switchGiving.isClickable = isClickable
             switchRespect.isClickable = isClickable
+            Log.e("EGO","HomeFragment-setSwitchesClickable isClickable: $isClickable")
         }
     }
 
@@ -186,6 +167,7 @@ class HomeFragment : Fragment() {
             switchKindness.isChecked = isChecked
             switchGiving.isChecked = isChecked
             switchRespect.isChecked = isChecked
+            Log.e("EGO","HomeFragment-setSwitchesChecked isChecked: $isChecked")
         }
     }
     private fun updateBottomNavigationView(items: List<Int> = emptyList()) {
@@ -194,6 +176,7 @@ class HomeFragment : Fragment() {
     }
     override fun onDestroyView() {
         super.onDestroyView()
+        Log.e("EGO","HomeFragment-onDestroyView")
         _binding = null
     }
 }
--- app/src/main/java/com/abdulkadirkara/emotions/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/abdulkadirkara/emotions/viewmodel/HomeFragmentViewModel.kt
@@ -1,5 +1,6 @@
 package com.abdulkadirkara.emotions.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -29,10 +30,12 @@ class HomeFragmentViewModel: ViewModel() {
         _areSwitchesClickable.value = false
         _areSwitchesChecked.value = false
         _maxItemsReachedMessage.value = false
+        Log.e("EGO", "HomeFragmentViewModel-init")
     }
 
     fun onEgoSwitchChanged(isChecked: Boolean) {
         _isEgoChecked.value = isChecked
+        Log.e("EGO", "HomeFragmentViewModel-onEgoSwitchChanged isChecked: $isChecked")
         if (isChecked) {
             handleEgoSwitchChecked()
         } else {
@@ -42,6 +45,7 @@ class HomeFragmentViewModel: ViewModel() {
 
     fun onOtherSwitchChanged(switchId: Int, isChecked: Boolean) {
         val items = _navigationItems.value ?: mutableListOf()
+        Log.e("EGO", "HomeFragmentViewModel-onOtherSwitchChanged switchId: $switchId isChecked: $isChecked")
         if (isChecked) {
             handleSwitchChecked(switchId, items)
         } else {
@@ -54,12 +58,14 @@ class HomeFragmentViewModel: ViewModel() {
         _maxItemsReachedMessage.value = false
     }
     private fun handleEgoSwitchChecked() {
+        Log.e("EGO", "HomeFragmentViewModel-handleEgoSwitchChecked")
         _areSwitchesClickable.value = false
         _areSwitchesChecked.value = false
         clearNavigationItems()
     }
 
     private fun handleEgoSwitchUnchecked() {
+        Log.e("EGO", "HomeFragmentViewModel-handleEgoSwitchUnchecked")
         _areSwitchesClickable.value = true
         setHomeFragmentAsDefaultNavigationItem()
     }
@@ -72,6 +78,7 @@ class HomeFragmentViewModel: ViewModel() {
         _navigationItems.value = mutableListOf(R.id.homeFragment)
     }
     private fun handleSwitchChecked(switchId: Int, items: MutableList<Int>) {
+        Log.e("EGO", "HomeFragmentViewModel-handleSwitchChecked switchId: $switchId , items.size: ${items.size}")
         if (!items.contains(switchId) && items.size < 5) {
             items.add(switchId)
         } else if (items.size >= 5) {
@@ -79,6 +86,7 @@ class HomeFragmentViewModel: ViewModel() {
         }
     }
     private fun handleSwitchUnchecked(switchId: Int, items: MutableList<Int>) {
+        Log.e("EGO", "HomeFragmentViewModel-handleSwitchUnchecked switchId: $switchId , items.size: ${items.size}")
         items.remove(switchId)
     }
 }
