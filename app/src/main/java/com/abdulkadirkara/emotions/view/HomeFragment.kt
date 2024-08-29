c07c6b2935a25595159956a1a85a3dd931523c0e
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-08-29 03:31:25 +0300

    HomeFragment warnings fixed

--- app/src/main/java/com/abdulkadirkara/emotions/view/HomeFragment.kt
+++ app/src/main/java/com/abdulkadirkara/emotions/view/HomeFragment.kt
@@ -1,17 +1,11 @@
 package com.abdulkadirkara.emotions.view
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Switch
 import android.widget.Toast
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.abdulkadirkara.emotions.R
 import com.abdulkadirkara.emotions.databinding.FragmentHomeBinding
 import com.abdulkadirkara.emotions.viewmodel.HomeFragmentViewModel
